8d1538bd5d9af9fb31e742a99ecdff3722df9c56
kferg357 <kennethferguson90@gmail.com>
2021-06-15 17:59:20 -0400

    fixed port issue

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+const PORT = process.env.PORT || 3030;
+
 
 
 app.use(express.urlencoded({ extended: true }));
